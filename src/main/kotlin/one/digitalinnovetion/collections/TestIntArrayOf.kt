d1ea0590c649ee2d0bf36efb16711ef7fd431b36
albertoamartinez <albertoazevedo112@gmail.com>
2021-07-11 23:48:44 -0300

    created int array test class

---
+++ src/main/kotlin/one/digitalinnovetion/collections/TestIntArrayOf.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovetion.collections
+
+fun main() {
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("-------------------------")
+
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+
+    println("-------------------------")
+
+}
